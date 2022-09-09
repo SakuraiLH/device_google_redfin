25a957966d12d99f5bc640dbaf9523108d525426
SakuraiLH <hikarilh@foxmail.com>
2022-09-09 23:26:29 +0000

    redfin: Import basic makefile for kogasakyokai
    Signed-off-by: SakuraiLH <hikarilh@foxmail.com>

---
+++ device-kogasa.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2022 KogasaKyokai
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/google/redbull/device-kogasa.mk)
---
+++ kogasa_redfin.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2022 KogasaKyokai
+# 
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit our common makefile.
+$(call inherit-product, vendor/kogasa/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/redfin/aosp_redfin.mk)
+
+include device/google/redfin/device-kogasa.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 5
+PRODUCT_NAME := kogasa_redfin
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=redfin \
+    PRIVATE_BUILD_DESC="redfin-user 13 TP1A.220905.004 8927612 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:13/TP1A.220905.004/8927612:user/release-keys
