b8d70e51e80993090ab6dc3575f9f6ead72e1420
SakuraiLH <hikarilh@foxmail.com>
2022-10-02 15:45:06 +0800

    redfin: Import MBN ISP files for China carriers
    Signed-off-by: SakuraiLH <hikarilh@foxmail.com>

--- device-kogasa.mk
+++ device-kogasa.mk
@@ -9,3 +9,7 @@ $(call inherit-product, device/google/redbull/device-kogasa.mk)
 # HBM
 PRODUCT_PACKAGES += \
     HbmSVManagerOverlay
+
+# MBN
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/google/redfin/mbn,$(TARGET_COPY_OUT_VENDOR)/rfs/msm/mpss/readonly/vendor/mbn)
